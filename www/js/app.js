d90e3942239ceb9fd66fb86f0701057d2448513e
Jackmew <wonderfuljack123@gmail.com>
2015-11-25 10:15:59 +0800

    dynamic create slides

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,20 @@
-// Ionic Starter App
+(function() {
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.controller('SlideCtrl', function($scope) {
+
+  $scope.slides = [] ;
+  for(var i = 0 ; i < 5 ; i++) {
+    $scope.slides.push({
+      title: 'Slider #' + i ,
+      content: 'Slider content #' + i 
+    });
+  }
+
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +26,5 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+}());
