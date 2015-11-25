42da585642be5683a4f1d4b3cff65798587ef6e7
Jackmew <wonderfuljack123@gmail.com>
2015-11-25 10:35:15 +0800

    add colors

--- www/js/app.js
+++ www/js/app.js
@@ -4,13 +4,23 @@ var app = angular.module('starter', ['ionic'])
 
 app.controller('SlideCtrl', function($scope) {
 
-  $scope.slides = [] ;
-  for(var i = 0 ; i < 5 ; i++) {
-    $scope.slides.push({
-      title: 'Slider #' + i ,
-      content: 'Slider content #' + i 
-    });
-  };
+  $scope.slides = [
+    {
+      title: 'Slider #1' ,
+      content: 'Content #1',
+      bgColor: 'rgb(67,135,241)'
+    },
+    {
+      title: 'Slider #2' ,
+      content: 'Content #2',
+      bgColor: 'rgb(232,234,99)'
+    },
+    {
+      title: 'Slider #3' ,
+      content: 'Content #3',
+      bgColor: 'rgb(235,101,235)'
+    }
+  ];
 
   $scope.activeSlide = 0 ;
   $scope.setSlide = function(index) {
